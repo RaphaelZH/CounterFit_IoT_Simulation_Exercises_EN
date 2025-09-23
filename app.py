f966f585e1c5c9e73c19d0341112e77a31ea1a6e
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-23 14:30:14 -0400

    light_level

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import time
 from counterfit_connection import CounterFitConnection
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
 
 CounterFitConnection.init('127.0.0.1',5000)
+
+light_sensor = GroveLightSensor(0)
+while True:
+    light_level = light_sensor.light
+    print("Light Level: ", light_level)
+    #CounterFitConnection.send_data("light_level", light_level)
+    time.sleep(1)
