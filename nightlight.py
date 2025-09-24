8f8c1fe4d1aefc58a6b965072ba0a8f5753e523e
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-24 01:06:40 -0400

    GroveLed

--- nightlight.py
+++ nightlight.py
@@ -1,12 +1,20 @@
 import time
+
 from counterfit_connection import CounterFitConnection
 from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
+from counterfit_shims_grove.grove_led import GroveLed
 
 CounterFitConnection.init('127.0.0.1',5000)
 
 light_sensor = GroveLightSensor(0)
+led = GroveLed(5)
+
 while True:
     light_level = light_sensor.light
+    if light_level < 300:
+        led.on()
+    else:
+        led.off()
     print("Light Level: ", light_level)
     #CounterFitConnection.send_data("light_level", light_level)
     time.sleep(1)
