1380c8e28594f20002166e9acc7af7a676c3654c
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-26 22:29:57 -0400

    commands

--- nightlight-server/app.py
+++ nightlight-server/app.py
@@ -4,9 +4,10 @@ import paho.mqtt.client as mqtt
 
 import json
 
-id = "iot_001_"
-client_name = f"{id}nightlight_server"
-client_telemetry_topic = f"{id}nightlight/telemetry"
+id = "iot_001"
+client_name = f"{id}_nightlight_server"
+client_telemetry_topic = f"{id}_nightlight/telemetry"
+server_command_topic = f"{id}_nightlight/commands/{id}"
 
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
 mqtt_client.connect("test.mosquitto.org", 1883, 60)
@@ -18,6 +19,11 @@ print("MQTT connected!")
 def handle_telemetry(client, userdata, message):
     payload = json.loads(message.payload.decode())
     print("Message received:", payload)
+
+    command = {"led_on": payload["light_level"] < 300}
+    print("Sending command:", command)
+
+    mqtt_client.publish(server_command_topic, json.dumps(command))
 
 
 mqtt_client.subscribe(client_telemetry_topic)
--- nightlight/app.py
+++ nightlight/app.py
@@ -13,9 +13,10 @@ CounterFitConnection.init("127.0.0.1", 5000)
 light_sensor = GroveLightSensor(0)
 led = GroveLed(5)
 
-id = "iot_001_"
-client_name = f"{id}nightlight_client"
-client_telemetry_topic = f"{id}nightlight/telemetry"
+id = "iot_001"
+client_name = f"{id}_nightlight_client"
+client_telemetry_topic = f"{id}_nightlight/telemetry"
+server_command_topic = f"{id}_nightlight/commands/{id}"
 
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
 mqtt_client.connect("test.mosquitto.org", 1883, 60)
@@ -23,12 +24,23 @@ mqtt_client.connect("test.mosquitto.org", 1883, 60)
 mqtt_client.loop_start()
 print("MQTT connected!")
 
-while True:
-    light_level = light_sensor.light
-    if light_level < 300:
+
+def handle_telemetry(client, userdata, message):
+    payload = json.loads(message.payload.decode())
+    print("Message received:", payload)
+
+    if payload["led_on"]:
         led.on()
     else:
         led.off()
+
+
+mqtt_client.subscribe(client_telemetry_topic)
+mqtt_client.on_message = handle_telemetry
+
+while True:
+    light_level = light_sensor.light
+
     telemetry = json.dumps({"light_level": light_level})
 
     print("Sending telemetry: ", telemetry)
