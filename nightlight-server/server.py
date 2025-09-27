c8f7fe32a24da3ac4173c7e2341f8bce8088dfee
Hao ZHANG <32324353+RaphaelZH@users.noreply.github.com>
2025-09-26 23:55:21 -0400

    two actuators

--- nightlight-server/server.py
+++ nightlight-server/server.py
@@ -5,9 +5,10 @@ import paho.mqtt.client as mqtt
 import json
 
 id = "iot_001"
-client_name = f"{id}_nightlight_server"
-client_telemetry_topic = f"{id}_nightlight/telemetry"
-server_command_topic = f"{id}_nightlight/commands/{id}"
+client_name = f"{id}/nightlight_server"
+client_telemetry_topic = f"{id}/nightlight/telemetry"
+server_command_topic_1 = f"{id}/nightlight/commands/actuator_1"
+server_command_topic_2 = f"{id}/nightlight/commands/actuator_2"
 
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
 mqtt_client.connect("test.mosquitto.org", 1883, 60)
@@ -23,7 +24,8 @@ def handle_telemetry(client, userdata, message):
     command = {"led_on": payload["light_level"] < 300}
     print("Sending command:", command)
 
-    mqtt_client.publish(server_command_topic, json.dumps(command))
+    mqtt_client.publish(server_command_topic_1, json.dumps(command))
+    mqtt_client.publish(server_command_topic_2, json.dumps(command))
 
 
 mqtt_client.subscribe(client_telemetry_topic)
--- nightlight/actuator_1.py
+++ nightlight/actuator_1.py
@@ -1,7 +1,6 @@
 import time
 
 from counterfit_connection import CounterFitConnection
-from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
 from counterfit_shims_grove.grove_led import GroveLed
 
 import paho.mqtt.client as mqtt
@@ -10,13 +9,12 @@ import json
 
 CounterFitConnection.init("127.0.0.1", 5000)
 
-light_sensor = GroveLightSensor(0)
-led = GroveLed(5)
+led = GroveLed(1)
 
 id = "iot_001"
-client_name = f"{id}_nightlight_client"
-client_telemetry_topic = f"{id}_nightlight/telemetry"
-server_command_topic = f"{id}_nightlight/commands/{id}"
+client_name = f"{id}/nightlight_client_2"
+client_telemetry_topic = f"{id}/nightlight/telemetry"
+server_command_topic = f"{id}/nightlight/commands/actuator_1"
 
 mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_name)
 mqtt_client.connect("test.mosquitto.org", 1883, 60)
@@ -35,16 +33,8 @@ def handle_telemetry(client, userdata, message):
         led.off()
 
 
-mqtt_client.subscribe(client_telemetry_topic)
+mqtt_client.subscribe(server_command_topic)
 mqtt_client.on_message = handle_telemetry
 
 while True:
-    light_level = light_sensor.light
-
-    telemetry = json.dumps({"light_level": light_level})
-
-    print("Sending telemetry: ", telemetry)
-
-    mqtt_client.publish(client_telemetry_topic, telemetry)
-
     time.sleep(5)
